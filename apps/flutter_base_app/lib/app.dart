9c8e58c093272b08564b4b578180eee055c97ef5
onishi taisei <taisei.developer@gmail.com>
2024-10-10 01:28:10 +0900

    🔧 chore: add screen_util package for responsive design

--- apps/flutter_base_app/lib/app.dart
+++ apps/flutter_base_app/lib/app.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_base_i18n/gen/strings.g.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 import 'core/providers/loading_notifier.dart';
 import 'core/providers/navigator_key.dart';
@@ -14,38 +15,45 @@ final class App extends ConsumerWidget {
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     final goRouter = ref.watch(goRouterProvider);
-    return MaterialApp.router(
-      routerConfig: goRouter,
-      locale: TranslationProvider.of(context).flutterLocale,
-      supportedLocales: AppLocaleUtils.supportedLocales,
-      localizationsDelegates: GlobalMaterialLocalizations.delegates,
-      debugShowCheckedModeBanner: false,
-      scaffoldMessengerKey: ref.watch(scaffoldMessengerKeyProvider),
-      builder: (context, child) => Consumer(
-        builder: (context, ref, _) {
-          final isLoading = ref.watch(loadingNotifierProvider);
 
-          return Navigator(
-            key: ref.watch(navigatorKeyProvider),
-            onDidRemovePage: (Page<Object?> page) => false,
-            pages: [
-              MaterialPage<Widget>(
-                child: Stack(
-                  children: [
-                    child!,
-                    if (isLoading)
-                      const ColoredBox(
-                        color: Colors.black26,
-                        child: Center(
-                          child: CircularProgressIndicator(),
+    return ScreenUtilInit(
+      minTextAdapt: true,
+
+      /// 画面サイズが確実に取得できるまで待つ。
+      ensureScreenSize: true,
+      builder: (context, _) => MaterialApp.router(
+        routerConfig: goRouter,
+        locale: TranslationProvider.of(context).flutterLocale,
+        supportedLocales: AppLocaleUtils.supportedLocales,
+        localizationsDelegates: GlobalMaterialLocalizations.delegates,
+        debugShowCheckedModeBanner: false,
+        scaffoldMessengerKey: ref.watch(scaffoldMessengerKeyProvider),
+        builder: (context, child) => Consumer(
+          builder: (context, ref, _) {
+            final isLoading = ref.watch(loadingNotifierProvider);
+
+            return Navigator(
+              key: ref.watch(navigatorKeyProvider),
+              onDidRemovePage: (Page<Object?> page) => false,
+              pages: [
+                MaterialPage<Widget>(
+                  child: Stack(
+                    children: [
+                      child!,
+                      if (isLoading)
+                        const ColoredBox(
+                          color: Colors.black26,
+                          child: Center(
+                            child: CircularProgressIndicator(),
+                          ),
                         ),
-                      ),
-                  ],
+                    ],
+                  ),
                 ),
-              ),
-            ],
-          );
-        },
+              ],
+            );
+          },
+        ),
       ),
     );
   }
